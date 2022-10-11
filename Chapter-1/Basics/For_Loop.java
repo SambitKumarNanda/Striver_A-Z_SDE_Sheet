d487aa6c58011081eca69718ce2425603145b55c
SambitKumarNanda <nanu2647542@gmail.com>
2022-10-11 23:29:34 +0530

    for loop added

---
+++ Chapter-1/Basics/For_Loop.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class For_Loop{
+    public static void main(String[] args) {
+       
+
+        // For Loop
+        // Prints numbers Starting from 0 to 9
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        
+    }
+}
---
+++ Chapter-1/Basics/While_Loop.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class While_Loop {
+    public static void main(String[] args) {
+        
+        // While Loop
+        // Prints numbers from 10 to 1
+        int i = 10; 
+        while(i > 0){
+            System.out.println(i);
+            i --;
+        }
+    }
+}
