4ef61be56f31035ea539bb7b471aa391d38a32f4
SambitKumarNanda <nanu2647542@gmail.com>
2022-10-12 21:55:15 +0530

    Pattern 1 to 5 added

---
+++ Chapter-1/Pattern/FirstPattern.java
@@ -0,0 +1,21 @@
+public class FirstPattern {
+    public static void main(String[] args) {
+        
+        // Pattern
+        /*
+            *****
+            *****
+            *****
+            *****
+            *****
+         */
+
+         for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println("\n");
+         }
+
+    }
+}
---
+++ Chapter-1/Pattern/SecondPattern.java
@@ -0,0 +1,21 @@
+public class SecondPattern {
+    public static void main(String[] args) {
+        
+        // Pattern
+        /*
+            *
+            **
+            ***
+            ****
+            *****
+         */
+
+         for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("\n");
+         }
+
+    }    
+}
