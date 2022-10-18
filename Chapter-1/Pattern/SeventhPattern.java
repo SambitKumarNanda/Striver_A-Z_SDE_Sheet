0c7d59137b62cf4a13ed215242f6ce00aedcdb56
SambitKumarNanda <nanu2647542@gmail.com>
2022-10-18 23:33:00 +0530

    test

--- Chapter-1/Pattern/SeventhPattern.java
+++ Chapter-1/Pattern/SeventhPattern.java
@@ -1,5 +1,6 @@
 public class SeventhPattern {
   public static void main(String args[]){
+    // PATTERN - 1
 
       /*
                 *
@@ -8,15 +9,13 @@ public class SeventhPattern {
           *  *  *  *  * *
       */
 
-      for(int i=1;i<=5;i++){
-        for (int j=5-i; j>1; j--)
-            {
-                System.out.print(" ");
-            }
-            for (int j=0; j<=i; j++ )
-            {
-                System.out.print("* ");
-            }
+      for(int i=1;i<5;i++){
+        for (int j=4 ; j <=i ; j++) {
+            System.out.print(" ");
+        }
+        for(int k=1;k<=i;k++){
+            System.out.print("* ");
+        }
         System.out.println();
         }
       }
