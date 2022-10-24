6f05079bd4622ccb8ef5aa054eac2c10c2ba7881
SambitKumarNanda <nanu2647542@gmail.com>
2022-10-24 23:29:20 +0530

    ninth pattern

---
+++ Chapter-1/Pattern/NinthPattern.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class NinthPattern{
+    public static void main(String[] args) {
+     Scanner scanner = new Scanner(System.in);   
+     int n = scanner.nextInt();
+     ninth_pattern(n);
+     scanner.close();
+    }
+
+    private static void ninth_pattern(int n) {
+
+    }
+
+}
