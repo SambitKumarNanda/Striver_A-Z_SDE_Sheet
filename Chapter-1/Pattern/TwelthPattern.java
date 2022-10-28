f678447fe88bc1651c6c73a240d9bcb3b7e1394a
SambitKumarNanda <nanu2647542@gmail.com>
2022-10-28 22:56:11 +0530

    Twelveth pattern added

---
+++ Chapter-1/Pattern/TwelthPattern.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class TwelthPattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        twelve_pattern(n);
+    }
+
+    private static void twelve_pattern(int n) {
+        for (int row = 0; row < n; row++) {
+            for (int gap = 0; gap < row; gap++) {
+                System.out.print("");
+            }
+            for (int col = 0; col < row; col++) {
+                System.out.println(col);
+            }
+        }
+    }
+}
