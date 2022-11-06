2aae2acf4c13ea6fa7099244047942d38ffe853a
SambitKumarNanda <nanu2647542@gmail.com>
2022-11-06 23:51:32 +0530

    eighteen pattern added

---
+++ Chapter-1/Pattern/EighteenthPattern.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class EighteenthPattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+
+        eighteen_pattern(n);
+    }
+
+    private static void eighteen_pattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print((char)('E' - j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
