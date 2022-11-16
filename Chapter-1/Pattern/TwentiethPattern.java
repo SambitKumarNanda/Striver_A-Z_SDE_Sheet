a52440d0070ec38a53ada07a89e9af656e235493
SambitKumarNanda <nanu2647542@gmail.com>
2022-11-16 23:05:27 +0530

    test

--- Chapter-1/Pattern/TwentiethPattern.java
+++ Chapter-1/Pattern/TwentiethPattern.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
-public class TwentiethPattern {
+public class TwentiethPattern {twenti
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int num = scanner.nextInt();
     }
 }
